49637ce2b8dc53b0ee83a9a1d19d91b75642de14
sosec11 <S.o.sec@outlook.com>
2026-03-01 23:48:48 +0100

    feat: add simple python commands

---
+++ my_program.py
@@ -0,0 +1,20 @@
+print("Ma première instruction Python!")
+
+x = 55
+name = "So"
+
+print(x)
+print(f"Je m'appelle {name}.")
+
+temperature = 60
+
+if temperature < 0 or temperature > 35:
+    print("Température anormale.")
+
+for i in range(5): # Parcourt les nombres de 0 à 4
+    print(f"Nombre : {i}")
+
+compteur = 0
+while compteur < 5: # Tant que
+    print (f"Comtpeur : {compteur}")
+    compteur += 1 # Incrémentation
