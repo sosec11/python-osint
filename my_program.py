229c2c744e3b529838fb9efdb5867642c207c18a
sosec11 <S.o.sec@outlook.com>
2026-03-02 23:59:07 +0100

    feat: nombre d'utilisateurs dans l'input

--- my_program.py
+++ my_program.py
@@ -1,20 +1,33 @@
-print("Ma première instruction Python!")
+print("Hello!")
 
-x = 55
-name = "So"
 
-print(x)
-print(f"Je m'appelle {name}.")
 
-temperature = 60
+print("Salut!")
+nombre_utilisateur = input("Combien y a-t-il de personnes avec toi? : ")
 
-if temperature < 0 or temperature > 35:
-    print("Température anormale.")
+# L'index (souvent abrégé "i") va permettre de compter le nombre d'itérations de la boucle
+index = 0
 
-for i in range(5): # Parcourt les nombres de 0 à 4
-    print(f"Nombre : {i}")
+# Boucle basée sur le nomnre d'utilisateurs
+# Par défaut, le type renvoyé par la fonction "input" est une "string"
+# Nous devons la convertir en int grâce à la syntaxe "int(variable)"
+while index < int(nombre_utilisateur):
+    nom_utilisateur = input("Comment t'appelles-tu? : ")
+    print(f"Enchanté {nom_utilisateur}, moi c'est Script!")
 
-compteur = 0
-while compteur < 5: # Tant que
-    print (f"Comtpeur : {compteur}")
-    compteur += 1 # Incrémentation
+    # Récupération de la longeur du prénom avec la fonction "len()"
+    longeur_prenom = len(nom_utilisateur)
+    if longeur_prenom > 5:
+    print(f"Ton prénom fait {longeur_prenom} caractères, c'est plutôt long!")
+else:
+    print(f"Ton prénom fait {longeur_prenom} caractères, c'est plutôt court!")
+
+    # Nous arrivons à la fin d'une boucle, nous devons incrémenter (ajouter 1)
+    # à l'index pour éviter une boucle infinie
+    index += 1
+
+# On adapte notre "au revoir" en fonction du nombre de personnes
+if int(nombre_utilisateur) > 1:
+    print("Au revoir tout le monde!")
+else:
+    print("Au revoir!")
