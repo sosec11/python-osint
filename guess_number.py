c1983ea21aed209f45527ac62dc0278c385e9ad5
sosec11 <S.o.sec@outlook.com>
2026-03-07 23:26:46 +0100

    feat: add basic math operations using Python Math module

--- guess_number.py
+++ guess_number.py
@@ -2,3 +2,15 @@ import math
 import random
 
 print(math.sqrt(16))
+
+# Calcul de la racine carrée
+nombre = 16
+racine = math.sqrt(nombre)
+print(f"La racine carrée de {nombre} est {racine}")
+
+# Utilisation de la constante (variable non modifiable) pi
+rayon = 5
+aire = math.pi * rayon**2
+
+print(f"L'aire d'un cercle de rayon {rayon} est {aire:.2f}")
+
