c7fc7b76cae7a5faed766f978cd14a09fdff46dc
sosec11 <S.o.sec@outlook.com>
2026-03-07 23:28:57 +0100

    feat: add basic math operations using Python Math module

--- guess_number.py
+++ guess_number.py
@@ -11,6 +11,9 @@ print(f"La racine carrée de {nombre} est {racine}")
 # Utilisation de la constante (variable non modifiable) pi
 rayon = 5
 aire = math.pi * rayon**2
-
 print(f"L'aire d'un cercle de rayon {rayon} est {aire:.2f}")
 
+# Calcul du factoriel
+n = 5
+factoriel = math.factorial(n)
+print(f"Le factoriel de {n} est {factoriel}")
