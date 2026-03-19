efc43f7b37b932885306aa58e60cc1478451fdb9
sosec11 <S.o.sec@outlook.com>
2026-03-19 14:06:57 +0100

    feat: simple python loop fix

--- version4.py
+++ version4.py
@@ -5,9 +5,9 @@ Pour sortir, il suffira de choisir la valeur 0.
 
 x=3 #il faut une valeur différente de 0 pour rentrer dans la boucle
 while(x!=0):
-x=int(input("Rentrer la valeur de x : "))
-resultat=x+2
-resultat=resultat*2
-print("Quand x vaut "+repr(x)+", le résultat vaut "+repr(resultat)+" !\n")
+    x=int(input("Rentrer la valeur de x : "))
+    resultat=x+2
+    resultat=resultat*2
+    print("Quand x vaut "+repr(x)+", le résultat vaut "+repr(resultat)+" !\n")
 
 print("Happy end !")
